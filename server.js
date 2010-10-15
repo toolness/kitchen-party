0a739a4d13aef44b0d76115479596ead662ed935
Atul Varma <varmaa@gmail.com>
2010-10-15 14:09:38 -0700

    Origination.

---
+++ server.js
@@ -0,0 +1,43 @@
+var http = require('http'),
+    io = require('./socket.io'),
+    fs = require('fs'),
+    sys = require('sys'),
+    url = require('url');
+
+var STATIC_FILES_DIR = './static-files'
+var INDEX_FILE = '/index.html';
+var STATIC_FILES = {
+  '/index.html': 'text/html'
+, '/jquery.min.js': 'application/javascript'
+};
+
+var server = http.createServer(function(req, res) {
+  var info = url.parse(req.url);
+  var path = info.pathname == '/' ? INDEX_FILE : info.pathname;
+
+  console.log("path", path);
+
+  if (path in STATIC_FILES) {
+    res.writeHead(200, {'Content-Type': STATIC_FILES[path]});
+    var file = fs.createReadStream(STATIC_FILES_DIR + path);
+    sys.pump(file, res);
+  } else {
+    res.writeHead(404, 'Not Found', {'Content-Type': 'text/plain'});
+    res.end('Not Found: ' + path);
+  }
+});
+
+var socket = io.listen(server, {
+  transports: [
+    'websocket'
+,   'server-events'
+,   'htmlfile'
+,   'xhr-multipart'
+,   'xhr-polling'
+  ]
+});
+
+var users = require('./users').build();
+
+socket.on('connection', users.onConnection);
+server.listen(8080);
---
+++ users.js
@@ -0,0 +1,60 @@
+function User(users, id, client) {
+  var self = this;
+
+  self.send = function send(obj) {
+    client.send(JSON.stringify(obj));
+  };
+
+  self.send({msg: 'init', id: id});
+
+  client.on('message', function(data) {
+    data = JSON.parse(data);
+
+    switch (data.msg) {
+      case 'say':
+        users.broadcast({
+          msg: 'say',
+          from: id,
+          content: data.content
+        });
+        break;
+      default:
+        console.warn("Unknown msg:", data.msg);
+        break;
+    }
+  });
+}
+
+function Users() {
+  var self = this;
+  var users = {};
+  var latestID = 0;
+
+  self.onConnection = function(client) {
+    var id = ++latestID;
+
+    self.broadcast({
+      msg: 'connect',
+      id: id
+    });
+
+    users[id] = new User(self, id, client);
+
+    client.on('disconnect', function() {
+      delete users[id];
+      self.broadcast({
+        msg: 'disconnect',
+        id: id
+      });
+    });
+  };
+  
+  self.broadcast = function(obj) {
+    for (var id in users)
+      users[id].send(obj);
+  };
+}
+
+exports.build = function build() {
+  return new Users();
+};
